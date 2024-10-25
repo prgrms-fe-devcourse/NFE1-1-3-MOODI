7f3973e1f80ecc9b7ddd73fe34a02cabe6b29b00
jymaeng <jymaeng1234@gmail.com>
2024-10-25 17:07:13 +0900

    feat : theme 객체 추가

---
+++ src/app/styles/theme.ts
@@ -0,0 +1,25 @@
+import { DefaultTheme } from 'styled-components'
+
+const theme: DefaultTheme = {
+    layout: {
+        max_width: '960px'
+    },
+    colors: {
+        black: '#000000',
+        gray_dark: '#676767',
+        gray_normal: '#00000066',
+        gray_light: '#BDBDBD',
+        orange_primary: '#FF480E',
+        orange_warn: '#FF4949',
+        orange_button: '#FF480E',
+        orange_reaction: '#FFBB80',
+        green: '#3BDE86',
+        white: '#FFFFFF',
+        white_bg: '#F8F8F8'
+    },
+    border: {
+        default: '1px solid #E0E0E0'
+    }
+}
+
+export default theme
