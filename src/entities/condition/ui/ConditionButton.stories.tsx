7a55e237f4b3d1170225ef45b1d413c9e6603f80
임채민 <87525734+cmlim0070@users.noreply.github.com>
2024-10-28 16:17:59 +0900

    rename : emotion -> condition (#42)

--- src/entities/condition/ui/ConditionButton.stories.tsx
+++ src/entities/condition/ui/ConditionButton.stories.tsx
@@ -1,15 +1,15 @@
 // features/diary-write/ui/EmotionButton/EmotionButton.stories.tsx
 import type { Meta, StoryObj } from '@storybook/react';
-import { EmotionButton } from './EmotionButton';
+import { ConditionButton } from './ConditionButton';
 
 const meta = {
- title: 'Entities/Emotion/EmotionButton',
- component: EmotionButton,
+ title: 'Entities/Condition/ConditionButton',
+ component: ConditionButton,
  parameters: {
    layout: 'centered',
  },
  tags: ['autodocs'],
-} satisfies Meta<typeof EmotionButton>;
+} satisfies Meta<typeof ConditionButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -18,7 +18,7 @@ export const Active: Story = {
  args: {
    children: '좋음',
    isActive: true,
-   onClick: () => alert('보통 감정 클릭'),
+   onClick: () => alert('좋음'),
  },
 };
 
@@ -26,6 +26,6 @@ export const Inactive: Story = {
  args: {
    children: '나쁨',
    isActive: false,
-   onClick: () => alert('나쁨 감정 클릭!'),
+   onClick: () => alert('나쁨'),
  },
 };
---
+++ src/feature/diary-write/condition/ui/ConditionButtonGroup.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container } from './ConditionButtonGroup.styled';
+import { ConditionButtonGroupProps, CONDITIONS } from '../model/type';
+import { ConditionButton } from '../../../../entities/condition';
+
+// condition으로 네이밍 변경 해야합니다
+export const ConditionButtonGroup = ({
+    selectedCondition, // 현재 선택되어있는 기분 상태
+    onChange // 부모 컴포넌트로 선택된 기분 전달
+}: ConditionButtonGroupProps) => {
+    return (
+        <Container>
+            {CONDITIONS.map((condition) => {
+                return (
+                    <ConditionButton
+                        key={condition}
+                        isActive={selectedCondition === condition}
+                        onClick={() => onChange(condition)}
+                    >
+                        {condition}
+                    </ConditionButton>
+                );
+            })}
+        </Container>
+    );
+};
