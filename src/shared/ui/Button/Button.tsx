194c67d43a952f1fbcfe117eb75870fd2c8cfb97
kangminguu <mingudat9909@gmail.com>
2024-10-29 09:44:33 +0900

    fix : 공유 컴포넌트 타입 분리 및 오류 수정

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import { StyledButton } from './Button.styled';
-
-export interface ButtonProps {
-    children: React.ReactNode;
-    fontSize: string;
-    isActive: boolean;
-    hasBorder: boolean;
-    height: string;
-    width: string;
-    onClick: () => void;
-}
+import { ButtonProps } from './Button.types';
 
 const Button = ({
     children,
