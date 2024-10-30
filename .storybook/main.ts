90934d616dd88e4963747f6dff1606d6f08abcba
chaemin <87525734+cmlim0070@users.noreply.github.com>
2024-10-30 18:30:12 +0900

    env : 스토리북 설정 / 로딩스피너

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -13,6 +14,19 @@ const config: StorybookConfig = {
     framework: {
         name: '@storybook/react-webpack5',
         options: {}
+    },
+    webpackFinal: async (webpackConfig) => {
+        return {
+            ...webpackConfig,
+            resolve: {
+                ...webpackConfig.resolve,
+                alias: {
+                    ...webpackConfig.resolve?.alias,
+                    '@': path.resolve(__dirname, '../src')
+                },
+                extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+            }
+        };
     },
     staticDirs: ['../public']
 };
