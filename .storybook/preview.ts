18f23c67e334d28be36cd592737d03c10045a535
chaemin <87525734+cmlim0070@users.noreply.github.com>
2024-10-30 14:51:49 +0900

    feat : 스포티파이&유튜브 데이터 패치 로직 작성

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import type { Preview } from '@storybook/react';
+import { initialize, mswLoader } from 'msw-storybook-addon';
+
+// MSW 초기화
+initialize();
 
 const preview: Preview = {
     parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
         controls: {
             matchers: {
                 color: /(background|color)$/i,
-                date: /Date$/i
+                date: /Date$/
             }
         }
-    }
+    },
+    loaders: [mswLoader]
 };
 
 export default preview;
