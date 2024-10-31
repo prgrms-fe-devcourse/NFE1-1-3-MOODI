ca8c7f84880b1d3fba27a22b97b57dcf734fdd50
kangminguu <mingudat9909@gmail.com>
2024-10-31 14:03:56 +0900

    feat : emotion select

---
+++ src/features/diary-write/emotion/ui/EmotionButtonGroup.styled.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const EmotionButtonContainer = styled.div`
+    max-width: 960px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+`;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    gap: 30px;
+`;
+
+export const MainKeywordContainer = styled.div`
+    font-size: 14px;
+    font-family: 'Pretendard', sans-serif;
+    color: #8c8c8c;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 0.5rem;
+`;
+
+export const SubKeywordContainer = styled.div`
+    font-size: 14px;
+    font-family: 'Pretendard', sans-serif;
+    color: #8c8c8c;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    gap: 0.5rem;
+`;
+
+export const SubContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: start;
+    gap: 0.5rem;
+`;
