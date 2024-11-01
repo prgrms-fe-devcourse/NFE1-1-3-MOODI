897ad475c01fbb864076115bf2c49778d4d9912d
MJK <annawa6@naver.com>
2024-11-01 12:47:39 +0900

    feat : 메인페이지 개발

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import GlobalStyles from './styles/globalStyles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ToastContainer from '@/features/Toast/ui/ToastContainer';
+import MainPage from '@/pages/MainPage';
 
 const queryClient = new QueryClient();
 
@@ -8,6 +10,8 @@ const App: React.FC = () => {
     return (
         <QueryClientProvider client={queryClient}>
             <GlobalStyles />
+            <ToastContainer />
+            <MainPage />
         </QueryClientProvider>
     );
 };
