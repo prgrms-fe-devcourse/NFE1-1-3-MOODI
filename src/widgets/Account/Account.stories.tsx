98330c483ba999c3c60bf0118e50aa4324e8fc80
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-01 17:12:39 +0900

    feat : 회원가입/아이디 위젯 개발

---
+++ src/widgets/Account/Account.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Account from './Account';
+
+const meta: Meta<typeof Account> = {
+  component: Account,
+  title: 'Widgets/ui/Account',
+  tags: ['autodocs'],
+  argTypes: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Account>;
+
+export const Default: Story = {
+  args: {},
+};
