4165b9465e7dab4d52baf67dadbae823170e88e8
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-02 03:13:30 +0900

    chore : 헤더 제거

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,4 +1,3 @@
-import Header from '@/widgets/header/ui/Header';
 import CalendarContainer from './container/calendar-container/CalendarContainer';
 import TimelineContainer from './container/timeline-container/TimelineContainer';
 import MoveTopButton from '@/shared/ui/MoveTopButton/MoveTopButton';
@@ -6,7 +5,6 @@ import MoveTopButton from '@/shared/ui/MoveTopButton/MoveTopButton';
 const MainPage = () => {
     return (
         <>
-            <Header />
             <CalendarContainer />
             <TimelineContainer />
             <MoveTopButton />
