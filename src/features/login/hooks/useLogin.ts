158ebc2e3bb582c953587bea24e0e0242e5882f4
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-04 14:54:18 +0900

    feat : 로그인 하는 훅 구현

--- src/features/login/hooks/useLogin.ts
+++ src/features/login/hooks/useLogin.ts
@@ -3,15 +3,29 @@ import { useMutation, UseMutationResult } from '@tanstack/react-query';
 import { LoginType } from '../model/LoginType';
 import { login } from '@/shared/api/login';
 import { useToastStore } from '@/features/Toast/hooks/useToastStore';
+import useLocalStorage from '@/shared/hooks/useLocalStorage';
+import { useNavigate } from 'react-router-dom';
+import { decodeJwtPayload } from '../util/decodeJwtPayload ';
+import { useAuthStore } from './useAuthStore';
+
+interface userType {
+    email: string;
+    username: string;
+}
 
 const useLogin = (): UseMutationResult<LoginType, Error, LoginParams> => {
+    const navigate = useNavigate();
     const { addToast } = useToastStore();
-
+    const { setValue: setToken } = useLocalStorage<string>('token', '');
+    const { email, userName, setUserInfo } = useAuthStore();
     const mutation = useMutation<LoginType, Error, LoginParams>({
         mutationFn: (params: LoginParams) => login(params),
         onSuccess: (res: LoginType) => {
             addToast('로그인 성공했습니다.', 'success');
-            return res;
+            setToken(res.token);
+            const payload = decodeJwtPayload<userType>(res.token);
+            setUserInfo(email, userName, true);
+            navigate('/');
         },
         onError: (err: Error) => {
             addToast(err.message, 'warning');
