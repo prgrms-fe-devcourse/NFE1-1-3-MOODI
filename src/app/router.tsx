10e0490841be6a5f5e02fc188046a5f116e26b9e
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-04 15:03:30 +0900

    chore : 라우팅 공백제거

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,9 +1,7 @@
 import MainPage from '@/pages/MainPage';
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './layout/Layout';
-
 import MyDiaryListPage from '@/pages/MyDiaryListPage';
-
 import LoginPage from '@/pages/LoginPage/LoginPage';
 
 const router = createBrowserRouter([
