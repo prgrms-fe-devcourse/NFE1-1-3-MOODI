639c87b6a5390f5323f962d288f31da211616129
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-04 11:20:54 +0900

    feat : 라우팅 설정

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,10 @@
 import MainPage from '@/pages/MainPage';
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './layout/Layout';
+
 import MyDiaryListPage from '@/pages/MyDiaryListPage';
+
+import LoginPage from '@/pages/LoginPage/LoginPage';
 
 const router = createBrowserRouter([
     {
@@ -13,7 +16,11 @@ const router = createBrowserRouter([
                 element: <MainPage />
             },
             {
-                path: '/my-diary',
+                path: '/login',
+                element: <LoginPage />
+            },
+            {
+                path: 'diary',
                 element: <MyDiaryListPage />
             }
         ]
