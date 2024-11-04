47d6986bed7fb0f0fe5443618a1a7ecb59922580
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-05 01:48:35 +0900

    feat : 유효성 검사

---
+++ src/widgets/Sign/util/RegExp.ts
@@ -0,0 +1,8 @@
+export const EMAIL_REG_EXP: RegExp =
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+export const PHONE_NUMBER_REG_EXP: RegExp =
+    /^(01[016789]-?\d{3,4}-?\d{4})$|^(0[2-8][0-5]?-?\d{3,4}-?\d{4})$/;
+
+export const PASSWORD_REG_EXP: RegExp =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+~`\-={}[\]:;"'<>,.?/]).{8,20}$/;
