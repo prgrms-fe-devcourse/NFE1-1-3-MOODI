6175483a5942d3399b2a189d92e9addfbc58c2fb
chaemin <87525734+cmlim0070@users.noreply.github.com>
2024-11-05 17:00:53 +0900

    feat : 로그아웃 & AuthProvider 추가

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,38 +1,14 @@
-import { useAuthStore } from '@/features/login/hooks/useAuthStore';
-import { decodeJwtPayload } from '@/features/login/util/decodeJwtPayload ';
-import useLocalStorage from '@/shared/hooks/useLocalStorage';
 import Header from '@/widgets/header/ui/Header';
-import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-
-interface UserInfo {
-    email: string;
-    userName: string;
-    isLoggedin: boolean;
-}
-
-interface userType {
-    email: string;
-    username: string;
-}
+import AuthProvider from '../AuthProvider';
 
 const Layout = () => {
-    const { setUserInfo } = useAuthStore();
-    const { storedValue } = useLocalStorage<string>('token', '');
-
-    useEffect(() => {
-        if (storedValue) {
-            const payload = decodeJwtPayload<userType>(storedValue);
-            if (payload) {
-                setUserInfo(payload.email, payload.username, true);
-            }
-        }
-    }, [setUserInfo, storedValue]);
-
     return (
         <div>
-            <Header />
-            <Outlet />
+            <AuthProvider>
+                <Header />
+                <Outlet />
+            </AuthProvider>
         </div>
     );
 };
