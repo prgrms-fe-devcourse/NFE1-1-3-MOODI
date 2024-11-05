f552142c6b75e5a26b596a27707552f9297cc171
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-05 09:18:35 +0900

    feat : 라우팅 수정

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,6 +4,7 @@ import Layout from './layout/Layout';
 import MyDiaryListPage from '@/pages/MyDiaryListPage';
 import LoginPage from '@/pages/LoginPage/LoginPage';
 import SignPage from '@/pages/SignPage/SignPage';
+import MyPage from '@/pages/MyPage/MyPage';
 
 const router = createBrowserRouter([
     {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
             {
                 path: '/join',
                 element: <SignPage />
+            },
+            {
+                path: '/my-page',
+                element: <MyPage />
             }
         ]
     }
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -21,7 +21,11 @@ const Header = () => {
                         <NavItem>내 일기</NavItem>
                     </Link>
                 )}
-                {isLoggedin && <NavItem>마이페이지</NavItem>}
+                {isLoggedin && (
+                    <Link to="/my-page">
+                        <NavItem>마이페이지</NavItem>
+                    </Link>
+                )}
                 {!isLoggedin && (
                     <Link to="/login">
                         <NavItem>로그인</NavItem>
