f83c0b850394a01f230a368159f642d695a18add
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-11-05 15:25:22 +0900

    feat : api 연동

---
+++ src/pages/AccountPage/AccountPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Account from '@/widgets/Account/Account';
+
+const AccountPage = () => {
+    return (
+        <div>
+            <Account />
+        </div>
+    );
+};
+
+export default AccountPage;
