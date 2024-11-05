5340c279533524144dabe879a8dba3fd55aaa169
chaemin <87525734+cmlim0070@users.noreply.github.com>
2024-11-05 16:07:43 +0900

    feat : Layout에서 로그인 상태를 처리할 수 있는 로직 작성

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,8 +1,34 @@
+import { useAuthStore } from '@/features/login/hooks/useAuthStore';
+import { decodeJwtPayload } from '@/features/login/util/decodeJwtPayload ';
+import useLocalStorage from '@/shared/hooks/useLocalStorage';
 import Header from '@/widgets/header/ui/Header';
-import React from 'react';
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
+interface UserInfo {
+    email: string;
+    userName: string;
+    isLoggedin: boolean;
+}
+
+interface userType {
+    email: string;
+    username: string;
+}
+
 const Layout = () => {
+    const { setUserInfo } = useAuthStore();
+    const { storedValue } = useLocalStorage<string>('token', '');
+
+    useEffect(() => {
+        if (storedValue) {
+            const payload = decodeJwtPayload<userType>(storedValue);
+            if (payload) {
+                setUserInfo(payload.email, payload.username, true);
+            }
+        }
+    }, [setUserInfo, storedValue]);
+
     return (
         <div>
             <Header />
