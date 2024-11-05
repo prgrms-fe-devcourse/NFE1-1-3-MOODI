ece2ec1ad60a5fd1eda6b893dd61412e92876c9b
임채민 <87525734+cmlim0070@users.noreply.github.com>
2024-11-05 17:16:49 +0900

    feat : 새로고침 시 로그인, 로그아웃 상태 관리 추가 (#178)
    * feat : 로고 메인으로 연결
    
    * feat : Layout에서 로그인 상태를 처리할 수 있는 로직 작성
    
    * feat : 로그아웃 & AuthProvider 추가
    
    * feat : 헤더 로그아웃 아이템 추가

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -3,14 +3,24 @@ import { Container, Logo, Nav, NavItem, LoginButton } from './Header.styled';
 import LogoImage from '@/shared/assets/logo.svg';
 import { useAuthStore } from '@/features/login/hooks/useAuthStore';
 import { Link, useNavigate } from 'react-router-dom';
+import useLogout from '@/features/login/hooks/useLogout';
 
 const Header = () => {
     const navigate = useNavigate();
-    const { email, userName, isLoggedin, setUserInfo } = useAuthStore();
+    const { userName, isLoggedin } = useAuthStore();
+    const { handleLogout } = useLogout();
+
+    const handleLogoutClick = (e: React.MouseEvent) => {
+        e.preventDefault();
+        handleLogout();
+    };
+
     return (
         <Container>
             <Logo>
-                <img src={LogoImage} alt="logo" />
+                <Link to="/">
+                    <img src={LogoImage} alt="logo" />
+                </Link>
             </Logo>
             <Nav>
                 <Link to="/">
@@ -26,10 +36,12 @@ const Header = () => {
                         <NavItem>마이페이지</NavItem>
                     </Link>
                 )}
-                {!isLoggedin && (
+                {!isLoggedin ? (
                     <Link to="/login">
                         <NavItem>로그인</NavItem>
                     </Link>
+                ) : (
+                    <NavItem onClick={handleLogoutClick}>로그아웃</NavItem>
                 )}
             </Nav>
         </Container>
