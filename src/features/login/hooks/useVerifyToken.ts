2b540edbbb7da3cd0e5b3509116b633ea7f020da
임채민 <87525734+cmlim0070@users.noreply.github.com>
2024-11-06 00:46:19 +0900

    chore : 주석 제거

--- src/features/login/hooks/useVerifyToken.ts
+++ src/features/login/hooks/useVerifyToken.ts
@@ -20,13 +20,11 @@ const useVerifyToken = () => {
 
     const verifyToken = async (token: string) => {
         try {
-            console.log('Sending verify request with token:', token);
             const response = await api.get<VerifyResponse>('/verify', {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
             });
-            console.log('Verify API full response:', response);
             if ('decoded' in response.data) {
                 const { decoded } = response.data;
                 setUserInfo(decoded.email, decoded.username, true);
