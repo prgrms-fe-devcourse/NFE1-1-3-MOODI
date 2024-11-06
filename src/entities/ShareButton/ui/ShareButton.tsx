47f9b3ee4520da7e7a38e2c6ee07e9b6f21630e6
kangminguu <131148077+kangminguu@users.noreply.github.com>
2024-11-06 17:08:05 +0900

    feat : 공유버튼 클릭 시 토스트 메시지 (#221)

--- src/entities/ShareButton/ui/ShareButton.tsx
+++ src/entities/ShareButton/ui/ShareButton.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { StyledButton, StyledImg } from './ShareButton.styled';
 import share from '@/shared/assets/share.svg';
+import { useToastStore } from '@/features/Toast/hooks/useToastStore';
 
 export const ShareButton = () => {
+    const { addToast } = useToastStore();
+
     /** 현재 페이지의 링크를 복사 */
     const handleCopyLink = () => {
         const currentUrl = window.location.href; // 현재 페이지 URL 가져오기
         navigator.clipboard
             .writeText(currentUrl)
             .then(() => {
-                console.log('복사 완료 :', currentUrl);
+                addToast('링크 복사 성공!', 'success');
             })
             .catch((error) => {
-                console.error('복사 실패 :', error);
+                addToast('링크 복사 실패!', 'warning');
             });
     };
 
