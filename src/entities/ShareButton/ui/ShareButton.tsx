75eb3a43205c04296b888eb6dfff4d31049a9c47
kangminguu <131148077+kangminguu@users.noreply.github.com>
2024-11-06 17:34:18 +0900

    fix : 링크 복사 방식 변경 (#229)

--- src/entities/ShareButton/ui/ShareButton.tsx
+++ src/entities/ShareButton/ui/ShareButton.tsx
@@ -9,14 +9,14 @@ export const ShareButton = () => {
     /** 현재 페이지의 링크를 복사 */
     const handleCopyLink = () => {
         const currentUrl = window.location.href; // 현재 페이지 URL 가져오기
-        navigator.clipboard
-            .writeText(currentUrl)
-            .then(() => {
-                addToast('링크 복사 성공!', 'success');
-            })
-            .catch((error) => {
-                addToast('링크 복사 실패!', 'warning');
-            });
+        const textArea = document.createElement('textarea');
+        textArea.value = currentUrl; // 텍스트 영역에 현재 URL 삽입
+        document.body.appendChild(textArea);
+        textArea.select(); // 텍스트 영역 선택
+        document.execCommand('copy'); // 선택한 텍스트 복사
+        document.body.removeChild(textArea); // 텍스트 영역 제거
+
+        addToast('링크 복사 성공!', 'success');
     };
 
     return (
