0ed8f86fffc0fc000a50957231c4caa8eea6c6a7
kangminguu <131148077+kangminguu@users.noreply.github.com>
2024-11-07 10:02:59 +0900

    style : 랜딩 페이지 수정 (#239)

--- src/pages/LandingPage/ui/Page/Page.tsx
+++ src/pages/LandingPage/ui/Page/Page.tsx
@@ -1,38 +1,41 @@
 import React from 'react';
 import {
+    Container,
     PageContainer,
     LandingContainer,
     SubTitleContainer
 } from './Page.styled';
 import LandingTitle from '../LandingTitle/LandingTitle';
 import SubTitle from '../SubTitle/SubTitle';
-import Button from '../../../../shared/ui/Button/Button';
-import Header from '../../../../widgets/header/ui/Header';
+import Button from '@/shared/ui/Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 export const LandingPage = () => {
+    const navigate = useNavigate();
+
     const handleLoginClick = () => {
-        // eslint-disable-next-line no-console
-        console.log('click login button!');
+        navigate(`/login`);
     };
 
     return (
-        <PageContainer>
-            <Header />
-            <LandingContainer>
-                <LandingTitle title={`Life's Playlist, \nYour Story`} />
-            </LandingContainer>
-            <SubTitleContainer>
-                <SubTitle subTitle="무디에서 감정을 담은 일기를 작성해보세요." />
-            </SubTitleContainer>
-            <Button
-                height="79px"
-                width="247px"
-                fontSize="20px"
-                borderradius="15px"
-                onClick={handleLoginClick}
-            >
-                로그인 하기
-            </Button>
-        </PageContainer>
+        <Container>
+            <PageContainer>
+                <LandingContainer>
+                    <LandingTitle title={`Life's Playlist, \nYour Story`} />
+                </LandingContainer>
+                <SubTitleContainer>
+                    <SubTitle subTitle="무디에서 감정을 담은 일기를 작성해보세요." />
+                </SubTitleContainer>
+                <Button
+                    height="79px"
+                    width="247px"
+                    fontSize="20px"
+                    borderradius="15px"
+                    onClick={handleLoginClick}
+                >
+                    로그인 하기
+                </Button>
+            </PageContainer>
+        </Container>
     );
 };
