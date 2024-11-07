07b1876e65d9a80ee2bc1e38ff7a61046e30960f
710minjoon <66014783+minjoon97@users.noreply.github.com>
2024-11-07 13:43:46 +0900

    fix : landingpage css수정 및 애니메이션 추가 (#258)

--- src/pages/LandingPage/ui/LandingTitle/LandingTitle.styled.ts
+++ src/pages/LandingPage/ui/LandingTitle/LandingTitle.styled.ts
@@ -1,14 +1,28 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-    font-size: 128px;
-    font-weight: bold;
+    font-size: 110px;
+    font-weight: 800;
     color: #000000;
     text-align: start;
-    line-height: 130px;
+    line-height: 115px;
+    letter-spacing: -2px;
     box-sizing: border-box;
     margin: 0;
     padding: 0;
     white-space: pre-wrap;
     font-family: 'Pretendard', sans-serif;
+    animation: revealTitle 1s ease-out forwards;
+    transform: translateY(100%); /* 초기 상태 설정 */
+
+    @keyframes revealTitle {
+        0% {
+            transform: translateY(10%);
+            opacity: 0; /* 선택적으로 페이드 인 효과 추가 */
+        }
+        100% {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
 `;
--- src/pages/LandingPage/ui/Page/Page.styled.ts
+++ src/pages/LandingPage/ui/Page/Page.styled.ts
@@ -5,11 +5,7 @@ export const Container = styled.div`
     margin: auto;
     top: 63px;
     height: calc(100vh - 63px);
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    padding-top: 200px;
 `;
 export const PageContainer = styled.div`
     width: 100%;
@@ -19,5 +15,5 @@ export const LandingContainer = styled.div`
     margin-bottom: 30px;
 `;
 export const SubTitleContainer = styled.div`
-    margin-bottom: 100px;
+    margin-bottom: 80px;
 `;
