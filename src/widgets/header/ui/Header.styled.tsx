2e5c9b039bc1e06c1d94d6589ec5a9f516225652
임채민 <87525734+cmlim0070@users.noreply.github.com>
2024-11-07 16:25:22 +0900

    style : 헤더 반응형 적용 (#268)

--- src/widgets/header/ui/Header.styled.tsx
+++ src/widgets/header/ui/Header.styled.tsx
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
+const breakpoints = {
+    mobile: '550px',
+    desktop: '1024px'
+};
+
+const media = {
+    mobile: `@media screen and (min-width: ${breakpoints.mobile})`,
+    desktop: `@media screen and (min-width: ${breakpoints.desktop})`
+};
+
 export const Container = styled.header`
     display: flex;
     flex-direction: row;
@@ -18,12 +28,19 @@ export const Logo = styled.div`
 `;
 
 export const Nav = styled.nav`
-    display: flex;
+    display: none;
     flex-direction: row;
     justify-content: center;
     align-items: center;
     gap: 1rem;
     font-size: 15px;
+
+    ${media.mobile} {
+        display: none;
+    }
+    ${media.desktop} {
+        display: flex;
+    }
 `;
 
 export const NavItem = styled.div`
@@ -45,4 +62,53 @@ export const LoginButton = styled.div`
     &:hover {
         background-color: #f2f2f2;
     }
+`;
+
+export const MobileNav = styled.div<{ isOpen: boolean }>`
+    display: ${(props) => (props.isOpen ? 'flex' : 'none')};
+    flex-direction: column;
+    position: fixed;
+    top: 0;
+    right: ${(props) => (props.isOpen ? '0' : '-100%')};
+    width: 50%;
+    height: 100vh;
+    background: white;
+    padding: 2rem;
+    box-shadow: -2px 0 5px rgba(0, 0, 0, 0.1);
+    transition: right 0.3s ease;
+    z-index: 8888;
+
+    ${media.desktop} {
+        display: none;
+    }
+`;
+
+export const MobileNavItems = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+    margin-top: 4rem;
+`;
+
+export const HamburgerButton = styled.button`
+    display: block;
+    background: none;
+    border: none;
+    cursor: pointer;
+    padding: 0 10px 0 10px;
+
+    ${media.desktop} {
+        display: none;
+        background: none;
+        border: none;
+    }
+`;
+
+export const CloseButton = styled.button`
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    background: none;
+    border: none;
+    cursor: pointer;
 `;
